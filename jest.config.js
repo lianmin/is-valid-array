3be274e71cfd74669310cf0b2c66628d774fa7e1
联民 <lianmin.slm@alibaba-inc.com>
2022-10-11 22:33:47 +0800

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['/node_modules/'],
+  verbose: false,
+};
+
